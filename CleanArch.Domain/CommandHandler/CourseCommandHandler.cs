f67306836c3dc345d1eaa8cda82bf1bb796de041
Dhinesh Viswa <dhineshviswa@outlook.com>
2022-07-11 21:53:47 +0530

    Commands, Handlers, updated course repository. Cleanedup IoC Container.

---
+++ CleanArch.Domain/CommandHandler/CourseCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using CleanArch.Domain.Commands;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+
+namespace CleanArch.Domain.CommandHandler
+{
+    public class CourseCommandHandler : IRequestHandler<CreateCourseCommand, bool>
+    {
+        public readonly ICourseRepository _courseRepositoty;
+
+        public CourseCommandHandler (ICourseRepository courseRepository)
+        {
+            _courseRepositoty = courseRepository;
+        }
+
+        public Task<bool> Handle(CreateCourseCommand request, CancellationToken cancellationToken)
+        {
+            var course = new Course()
+            {
+                Name = request.Name,
+                Description = request.Description,
+                ImageUrl = request.ImageUrl
+            };
+
+            _courseRepositoty.Add(course);
+
+            return Task.FromResult(true);
+        }
+    }
+}
